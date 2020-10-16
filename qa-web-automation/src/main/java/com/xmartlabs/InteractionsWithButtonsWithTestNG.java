35255ab976e5bfacd5df72c5f6ae09b28fb957a4
Ignacio Secco <isecco@guardanthealth.com>
2020-10-16 10:23:40 -0300

    Refactor testNG
    Implementation of annotations and adding a second test

--- qa-web-automation/src/main/java/com/xmartlabs/InteractionsWithButtonsWithTestNG.java
+++ qa-web-automation/src/main/java/com/xmartlabs/InteractionsWithButtonsWithTestNG.java
@@ -4,46 +4,99 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class InteractionsWithButtonsWithTestNG {
 
+    private WebDriver driver;
+
+
+    @BeforeSuite
+    public void initializeSuite(){
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        driver = new ChromeDriver();
+
+        System.out.println("Este texto se escribe desde el BeforeSuite.");
+    }
+
+    @AfterSuite
+    public void dropDownSuite(){
+        driver.close();
+        driver.quit();
+
+        System.out.println("Este texto se escribe desde el AfterSuite.");
+    }
+
+    @BeforeMethod
+    public void initializeTest(){
+        driver.get("https://demoqa.com/");
+
+        System.out.println("Este texto se escribe desde el BeforeMethod.");
+    }
+
+    @AfterMethod
+    public void dropDownTest(){
+
+        System.out.println("Mensage que se ejecuta luego de cada test");
+        
+        System.out.println("Este texto se escribe desde el AfterMethod.");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
     @Test
     public void simpleTest() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver");
-        WebDriver driver = new ChromeDriver();
 
-        driver.get("https://demoqa.com/");
         WebElement buttonElements = driver.findElement(By.xpath("//*[@id='app']/div/div/div[2]/div/div[1]/div/div[3]"));
         buttonElements.click();
-
-
         waiter(2000);
         //Llegamos a la pagina de "Elements"
-
         WebElement buttonRadioButton = driver.findElement(By.xpath("(//*[@id='item-2'])[1]"));
         buttonRadioButton.click();
 
         waiter(5000);
-
-
         //Comenzamos con la interacción con el radiobutton YES
         WebElement radioButtonYes = driver.findElement(By.xpath("//*[@id='app']/div/div/div[2]/div[2]/div[1]/div[2]/label"));
-        if (radioButtonYes.isSelected() == false){
+        if (radioButtonYes.isSelected() == true){
             System.out.println("El radio YES esta deseleccionado");
+            System.out.println("Este texto se escribe desde el Primer test.");
             radioButtonYes.click();
         }else{
             System.out.println("El radio YES esta seleccionad, lo cual esta MAL");
         }
-        
         waiter(5000);
+    }
 
+    @Test
+    public void simpleTest2() {
 
-        // Ejecucion de lo que estamos realizando, instrucciones, y etc.
-        driver.close();
-        driver.quit();
+        WebElement buttonElements = driver.findElement(By.xpath("//*[@id='app']/div/div/div[2]/div/div[1]/div/div[3]"));
+        buttonElements.click();
+        waiter(2000);
+        //Llegamos a la pagina de "Elements"
+        WebElement buttonRadioButton = driver.findElement(By.xpath("(//*[@id='item-2'])[1]"));
+        buttonRadioButton.click();
 
-
+        waiter(5000);
+        //Comenzamos con la interacción con el radiobutton YES
+        WebElement radioButtonYes = driver.findElement(By.xpath("//*[@id='app']/div/div/div[2]/div[2]/div[1]/div[2]/label"));
+        if (radioButtonYes.isSelected() == true){
+            System.out.println("El radio YES esta seleccionado");
+            System.out.println("Este texto se escribe desde el segundo test.");
+            radioButtonYes.click();
+        }else{
+            System.out.println("El radio YES esta seleccionad, lo cual esta MAL");
+        }
+        waiter(5000);
     }
 
 
